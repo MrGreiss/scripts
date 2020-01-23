2d99fe6d2fa58b80ba5bc38cd37ddb98a7e630c4
Jiri Dostal <j.dostal@mikroelektronika.cz>
2020-01-23 12:48:13 +0100

    removed debug prints and obsolete comments

--- BankingExport2YNAB.py
+++ BankingExport2YNAB.py
@@ -1,8 +1,6 @@
 import csv
 import sys
 
-print(sys.argv[0])
-print(sys.argv[1])
 if sys.argv[1].startswith("airbank"):
     columnMapping = (("Datum provedení", "Date"), ("Částka v měně účtu", "Inflow"),("Částka v měně účtu","Outflow"),("Název protistrany","Payee"), ("Poznámka k úhradě","Memo"))
 elif sys.argv[1].startswith("pohyby"):
@@ -12,14 +10,11 @@ else:
     exit()
 
 with open(sys.argv[1], 'r') as infile, open('ynab.csv', 'w') as outfile:
-    # output dict needs a list for new column ordering
     reader = csv.DictReader(infile, delimiter=';',)
-    print(reader.fieldnames)
     writer = csv.DictWriter(outfile, fieldnames=("Date","Payee","Memo","Outflow","Inflow"), lineterminator='\n', quoting=csv.QUOTE_ALL)
     writer.writeheader()
     for row in reader:
         outrow = {}
         for inkey, outkey in columnMapping:
             outrow.update({outkey: row[inkey]})
-        print(outrow)
         writer.writerow(outrow)
