419cca709b2fa81b10105a4f9ffd435a0acfc051
mvitek <marek.vitek@sap.com>
2020-08-04 17:33:28 +0200

    -- update with payyeess

--- BankingExport2YNAB.py
+++ BankingExport2YNAB.py
@@ -1,6 +1,26 @@
 import csv
 import sys
 
+
+def try_to_match_Payee(memo):
+    known_payees = \
+        {"BENZINA*": "Beznina",
+         "NETFLIX.COM": "Netflix",
+         "VODAFONE CZECH REP": "Vodafon",
+         "Alza.cz a.s": "Alza.cz",
+         "VOYO": "Voyo.sk",
+         }
+    payee_found = None
+    for payee in known_payees:
+        if payee in memo:
+            payee_found = known_payees[payee]
+            if payee_found is not None:
+                return payee_found
+
+    if payee_found is None:
+        raise KeyError("Missing match Payee")
+
+
 def trimIntro(filename, encoding):
     with open(filename, 'r', encoding=encoding) as file:
         lines = file.readlines()
@@ -10,25 +30,35 @@ def trimIntro(filename, encoding):
     with open(filename, 'w', encoding=encoding) as file:
         file.writelines(lines[index:])
 
+
 if sys.argv[1].startswith("airbank"):
-    columnMapping = (("Datum provedení", "Date"), ("Částka v měně účtu", "Inflow"),("Částka v měně účtu","Outflow"),("Název protistrany","Payee"), ("Poznámka k úhradě","Memo"))
+    columnMapping = (("Datum provedení", "Date"), ("Částka v měně účtu", "Inflow"), ("Částka v měně účtu", "Outflow"),
+                     ("Název protistrany", "Payee"), ("Poznámka k úhradě", "Memo"))
     inputFileEncoding = "cp1250"
     outputFileName = "ynab_airbank.csv"
-elif sys.argv[1].startswith("pohyby"):
-    columnMapping = (("datum zaúčtování","Date"),("částka","Outflow"),("částka","Inflow"),("název účtu protiúčtu","Payee"),("poznámka","Memo"))
+elif "pohyby" in sys.argv[1]:
+    columnMapping = (
+    ("datum zaúčtování", "Date"), ("částka", "Outflow"), ("částka", "Inflow"), ("název účtu protiúčtu", "Payee"),
+    ("poznámka", "Memo"))
     inputFileEncoding = "cp1250"
     outputFileName = "ynab_csob.csv"
     trimIntro(sys.argv[1], inputFileEncoding)
 else:
-    print("Unknown bank export file. Should start with: \"airbank\" (for AirBank export file) or \"pohyby\" (for CSOB export file).")
+    print(
+        "Unknown bank export file. Should start with: \"airbank\" (for AirBank export file) or \"pohyby\" (for CSOB export file).")
     exit()
 
-with open(sys.argv[1], 'r', encoding=inputFileEncoding) as infile, open(outputFileName, 'w', encoding='utf-8') as outfile:
-    reader = csv.DictReader(infile, delimiter=';',)
-    writer = csv.DictWriter(outfile, fieldnames=("Date","Payee","Memo","Outflow","Inflow"), lineterminator='\n', quoting=csv.QUOTE_ALL)
+with open(sys.argv[1], 'r', encoding=inputFileEncoding) as infile, open(outputFileName, 'w',
+                                                                        encoding='utf-8') as outfile:
+    reader = csv.DictReader(infile, delimiter=';', )
+    writer = csv.DictWriter(outfile, fieldnames=("Date", "Payee", "Memo", "Outflow", "Inflow"), lineterminator='\n',
+                            quoting=csv.QUOTE_ALL)
     writer.writeheader()
     for row in reader:
         outrow = {}
         for inkey, outkey in columnMapping:
-            outrow.update({outkey: row[inkey]})
+            if outkey is "Payee":
+                outrow.update({outkey: try_to_match_Payee(row["poznámka"])})
+            else:
+                outrow.update({outkey: row[inkey]})
         writer.writerow(outrow)
--- ExportLatestCsobFile.py
+++ ExportLatestCsobFile.py
@@ -1,4 +1,4 @@
 import glob, os
 files = glob.glob("pohyby*.csv")
 files.sort()
-os.system("BankingExport2YNAB.py \"" + files[-1] + "\"")
+os.system("/Users/i332916/Developer/scripts/BankingExport2YNAB.py \"" + files[-1] + "\"")
